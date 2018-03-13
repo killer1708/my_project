83a082c19991f48bf17cc65a669ad3e828f43989
killer1708 <rk415149@gmail.com>
2018-03-13 11:51:51 +0530

    This file is used for getting thearguments from command line terminal.

--- arg_parse.py
+++ arg_parse.py
@@ -5,8 +5,8 @@ class ArgParse(object):
 	def cl_arguments(self):
 		parser = argparse.ArgumentParser()
 		parser.add_argument("--input1", action="store", required=True, help="input filename")
-		parser.add_argument("--input2", action="store", help="output file, last n lines or first n lines")
-		parser.add_argument("--input3", action="store", help="flag")
+		parser.add_argument("--input2", action="store", help="output file(str) or last n lines(int) or first n lines(int)")
+		parser.add_argument("--input3", action="store", help="f or l")
 		args = parser.parse_args()
 		f_arg = args.input1
 		s_arg = args.input2
